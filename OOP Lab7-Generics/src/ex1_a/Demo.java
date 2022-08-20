7e99b1978058b15d0c095707165bfcf203f4903c
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-08-20 23:43:43 +0530

    create main class

--- OOP Lab7-Generics/src/ex1_a/Demo.java
+++ OOP Lab7-Generics/src/ex1_a/Demo.java
@@ -3,7 +3,21 @@ package ex1_a;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//crate an integer array with 10 element
+		
+		int arr[]= {45,3,66,23,100,12,1,49,76,22};
+		
+		//call the bubblesort in the main function
+		
+		Sort sort = new Sort();
+		
+		sort.bubbleSort(arr); //call parameter arry before we implement the Sort class 
+		
+		System.out.println("Sorted array: ");
+		for(int i=0; i<10; i++) {
+			System.out.println(arr[i]+"");
+		}
 
 	}
 
