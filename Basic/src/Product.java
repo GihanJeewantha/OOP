cc211ecea4210f54f077191cf292b3978d723965
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-01 22:13:06 +0530

    Update Product.java

--- Basic/src/Product.java
+++ Basic/src/Product.java
@@ -1,4 +1,28 @@
 
 public class Product {
+	
+	int pid;
+	String name;
+	int price;
+	
+	Product(){
+		System.out.println("this is constructor");
+	}
+	
+	void setProductDetails(int pid,String name,int price) {
+		this.pid=pid;
+		this.name=name;
+		this.price=price;
+	}
+	
+	void showProductDetails() {
+		System.out.println("product ID"+pid+"-----");
+		
+		System.out.println("Product Name"+name+"------");
+	}
+	
+	Product product = new Product();
+	
+	product.setProductDetails(101,"iPhone13",7000);
 
 }
