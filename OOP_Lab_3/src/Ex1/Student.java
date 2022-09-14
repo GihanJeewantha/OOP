0a9a175b9d37e6138430c16e0258b33f1175dce6
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-14 23:34:56 +0530

    OOP_Lab_3
    Create Student Class and Constructor

---
+++ OOP_Lab_3/src/Ex1/Student.java
@@ -0,0 +1,6 @@
+package Ex1;
+
+public class Student {
+	String name, ditno, address;
+
+}
---
+++ OOP_Lab_3/src/Ex1/Test.java
@@ -0,0 +1,28 @@
+package Ex1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Student S1 = new Student();
+		
+		S1.name = "Jeewantha";
+		S1.ditno = "123";
+		S1.address = "Anuradhapura";
+		
+		
+		Student S2 = new Student();
+		
+		S2.name =  "Dilshan";
+		S2.ditno = "456";
+		S2.address = "Malabe";
+		
+		System.out.println("Name "+S1.name+ "Dit NUmber "+S1.ditno+ "Address "+ S1.address);
+		
+		System.out.println("Name "+S2.name+ "Dit NUmber "+S2.ditno+ "Address "+ S2.address);
+
+	}
+
+}
