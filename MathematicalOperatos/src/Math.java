6769a46f2aaad1bdf942e8bab2254ac0f358b252
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-19 09:42:26 +0530

    Mathematical
    Mathematical Operators in java

---
+++ MathematicalOperatos/src/Math.java
@@ -0,0 +1,31 @@
+
+public class Math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// int x = 10;
+		// int y = 5;
+		
+		int x,y,z,a,b,c;
+		
+		x=10;
+		y=5;
+		
+		z = x+y;
+		
+		System.out.println("Answer " +z);
+		
+		a = x-y;
+		System.out.println("answer "+a);
+		
+		b = x/y;
+		System.out.println("answer "+b);
+		
+		c = x*y;
+		System.out.println("answer "+c);
+		
+
+	}
+
+}
