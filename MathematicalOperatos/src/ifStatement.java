adce94339f9e252825f405096caa6a31618a8553
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-19 11:07:49 +0530

    Create ifStatement.java
    if Else statement

---
+++ MathematicalOperatos/src/ifStatement.java
@@ -0,0 +1,16 @@
+
+public class ifStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 100;
+		
+		if(x == 10) {
+			System.out.println("True");
+		}else {
+			System.out.println("else");
+		}
+
+	}
+
+}
