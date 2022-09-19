a7176b3abaafb0f65a557072a486a930a5aecd6e
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-19 11:21:33 +0530

    Create SwitchCase.java

---
+++ MathematicalOperatos/src/SwitchCase.java
@@ -0,0 +1,32 @@
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int weekday = 2;
+		
+		switch(weekday) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		default  :
+			System.out.println("invalid input");
+				
+		}
+
+	}
+
+}
