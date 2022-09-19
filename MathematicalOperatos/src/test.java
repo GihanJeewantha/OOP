e2531ce344e7c49e82a02df8a33b31b238c1cb67
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-19 10:07:39 +0530

    Update test.java
    post increment and pre increment

--- MathematicalOperatos/src/test.java
+++ MathematicalOperatos/src/test.java
@@ -1,4 +1,24 @@
 
 public class test {
+	public static void main(String [] args) {
+		
+		
+		int x,y,a,b;
+		
+		x= 10;
+		
+		//post increment
+		y=x++;
+		System.out.println("y" +y);
+		System.out.println("x" +x);
+		
+		
+		a=10;
+		//pre increment
+		b=++a;
+		System.out.println("a"+a);
+		System.out.println("b"+b);
+		
+	}
 
 }
