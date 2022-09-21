54b89f8fda509c0eeef1c9ec043646430f11f476
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-21 23:52:20 +0530

    Update Array.java

--- Basic_Of_Java/src/Array.java
+++ Basic_Of_Java/src/Array.java
@@ -6,9 +6,11 @@ public class Array {
 
 		int[] numbers = {2,4,6,8};
 		String[] names = {"jeewantha","Dilshan","tharu"};
+		names[2] = "My Sister";
 		
 		System.out.println(names[0]);
 		System.out.println(numbers[2]);
+		System.out.println(names[2]);
 		
 	}
 	
