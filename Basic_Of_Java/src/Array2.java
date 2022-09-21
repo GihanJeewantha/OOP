1fde3d7ef7bc80c5b2d8be60611a2663297df4ab
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-21 23:55:50 +0530

    Create Array2.java
    Create array with limite

---
+++ Basic_Of_Java/src/Array2.java
@@ -0,0 +1,15 @@
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] names = new String[5];
+		names[0]="Windows";
+		names[1]="Apple";
+		
+		System.out.println(names[1]);
+
+	}
+
+}
