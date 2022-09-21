0890e7418ec938128d678de8e558ab6859fc2d79
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-21 23:50:37 +0530

    Basic_of_Java
    This is array basic of java Array

---
+++ Basic_Of_Java/src/Array.java
@@ -0,0 +1,16 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] numbers = {2,4,6,8};
+		String[] names = {"jeewantha","Dilshan","tharu"};
+		
+		System.out.println(names[0]);
+		System.out.println(numbers[2]);
+		
+	}
+	
+
+}
