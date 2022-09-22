b827b108d5cef1e39cfcd29012c8711cf0f2c2b9
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-23 00:52:38 +0530

    Create maintwo.java

---
+++ Basic_Of_Java/src/maintwo.java
@@ -0,0 +1,22 @@
+
+public class maintwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int x = 4;
+
+		   square(x);
+
+		   System.out.println(x);		
+
+		}
+
+		static void square(int x) {
+
+		   x = x*x;
+
+		}
+
+	}
+
+
