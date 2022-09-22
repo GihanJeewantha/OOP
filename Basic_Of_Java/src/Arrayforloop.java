a559b48f2b5b51e9a3989128c28cc82ae7bb4bc9
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-22 21:05:31 +0530

    Arraywithloop

---
+++ Basic_Of_Java/src/Arrayforloop.java
@@ -0,0 +1,14 @@
+
+public class Arrayforloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String [] names = {"jeewantha","dilshan", "369"};
+		for(int i = 0; i<=2; i++) {
+			System.out.println(names[i]);
+		}
+
+	}
+
+}
