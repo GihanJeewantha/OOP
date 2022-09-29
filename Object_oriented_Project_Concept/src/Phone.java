3db7d24ec6fba2120f77a9fe5ffc92eeaf4ee1aa
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-29 14:14:56 +0530

    Create Phone.java

---
+++ Object_oriented_Project_Concept/src/Phone.java
@@ -0,0 +1,16 @@
+
+public class Phone {
+	String name;
+	String color;
+	int ram;
+	
+	
+	public void call() {
+		System.out.println("Take a Call"+name);
+	}
+	
+	public void BrowseInternet() {
+		System.out.println("Browse  Internet...!"+color);
+	}
+
+}
