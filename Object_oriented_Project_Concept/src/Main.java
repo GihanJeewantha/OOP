9db8202b252c109b69351e2fb7d6c45633c22b5c
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-29 14:14:49 +0530

    Create Main.java

---
+++ Object_oriented_Project_Concept/src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//first Object
+		
+		Phone ph1 = new Phone();
+		
+		ph1.name = "Apple 14 Pro";
+		ph1.color = "Black";
+		ph1.ram = 16;
+		
+		ph1.call();
+		ph1.BrowseInternet();
+		
+		
+		//Second Object 
+		
+		Phone ph2 = new Phone();
+		ph1.name = "Apple 12 Pro";
+		ph1.color = "Gold";
+		ph1.ram = 16;
+		
+		ph1.call();
+		ph1.BrowseInternet();
+	}
+
+}
