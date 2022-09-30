26324608fc09074cba0b799980c254071d68a963
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-09-30 20:44:24 +0530

    Create Oldphone.java

---
+++ single inheritance/src/Oldphone.java
@@ -0,0 +1,19 @@
+
+public class Oldphone {
+	String name;
+
+	
+	void call() {
+		System.out.println("call");
+		
+	}
+	
+	void message() {
+		
+	}
+	
+	public static void main(String []args) {
+		Oldphone obj1 = new Oldphone();
+	
+	}
+}
