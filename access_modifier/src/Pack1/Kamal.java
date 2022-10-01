f0d7f9a90f6a01dd374581b78bf31a418a28846d
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-10-01 20:10:06 +0530

    Create Kamal.java
    Main method implement

---
+++ access_modifier/src/Pack1/Kamal.java
@@ -0,0 +1,12 @@
+package Pack1;
+import Pack2.Saman;
+
+public class Kamal {
+	public static void main (String []args) {
+		Nimal obj1 = new Nimal();
+		Saman obj2 = new Saman();
+		rajitha obj3 = new rajitha();
+		bun obj4 = new bun();
+	}
+
+}
