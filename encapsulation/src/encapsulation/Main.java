e5fc00fb619e073e8a17a61bb2816bd5b88695ed
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-10-09 20:05:36 +0530

    Encapsulation
    ussing getter and Setter

---
+++ encapsulation/src/encapsulation/Main.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class Main {
+	
+	public static void main(String[]args) {
+		Student obj = new Student();
+		obj.setName("Jeewantha");
+		
+		System.out.println(obj.getName());
+	}
+	
+	
+
+}
---
+++ encapsulation/src/encapsulation/Student.java
@@ -0,0 +1,16 @@
+package encapsulation;
+
+public class Student {
+	
+	private String name;
+	private int age;
+	
+	public void setName(String name) {
+		this.name = name;                  //take input
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
