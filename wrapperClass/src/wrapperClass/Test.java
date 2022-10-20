268963d138c2205ba4cd1b4d33906e1306a9cb90
GihanJeewantha <106438829+GihanJeewantha@users.noreply.github.com>
2022-10-20 22:07:50 +0530

    wrapper Class

---
+++ wrapperClass/src/wrapperClass/Test.java
@@ -0,0 +1,11 @@
+package wrapperClass;
+
+public class Test {
+	public static void main(String[]args) {
+		String x = "Jeewantha lk";
+		Integer y = 369;
+		double z = 3.6;
+		Boolean b = true;
+	}
+
+}
