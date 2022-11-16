c711157cf19a1eec8544171ac6b1824e7c50936b
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2022-11-16 22:08:22 +0530

    Lab 1 Ex 2
    print Student Information

---
+++ Lab1/src/Ex2/Student.java
@@ -0,0 +1,17 @@
+package Ex2;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("========================================");
+		System.out.println("=\tStudent Information\t\t\t=");
+		System.out.println("========================================");
+		System.out.println("=\tName \t:Kumarasingha I.M.K.B=");
+		System.out.println("=\tReg No \t\t:IT123456=");
+		System.out.println("=\tAddress \t:Anuradhapura=");
+		System.out.println("========================================");
+
+	}
+
+}
