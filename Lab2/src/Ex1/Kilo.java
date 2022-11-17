b6080beb7d77edd276e7b43fcc9a3f59ca45168f
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2022-11-17 14:33:53 +0530

    Lab 2 Ex 1
    Kilometer converter

---
+++ Lab2/src/Ex1/Kilo.java
@@ -0,0 +1,16 @@
+package Ex1;
+
+public class Kilo {
+	
+	public static void main(String[] args) {
+		int miles = 26 , yards = 385;
+		double kilometer;
+		// TODO Auto-generated method stub
+		kilometer = (miles*1.609)+(yards*1.609/1760);
+		System.out.println("kilometers : "+kilometer);
+		
+		
+
+	}
+
+}
